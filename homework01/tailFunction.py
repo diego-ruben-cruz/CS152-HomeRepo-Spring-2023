c01013fd326e7b035cf0ab51d76840b2b8a4491f
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-02-26 19:02:20 -0800

    Stuck on tail function for hw1, reading req

---
+++ homework01/tailFunction.py
@@ -0,0 +1,17 @@
+"""
+    Description:	
+    Python file for HW1.
+    Write a python script to write the tail for a file, 
+    given a user-defined number a lines to print out
+	
+	Name: Diego Cruz
+	SID: 013540384
+
+	Course: CS 152
+	Section: 01
+	Homework: HW1
+	Date: 27 Feb 2023
+
+    Using the following link as reference reading
+    <https://gist.github.com/amitsaha/5990310#file-tail_2-py>
+"""
