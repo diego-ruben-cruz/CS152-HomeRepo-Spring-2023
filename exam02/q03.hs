ddc2b71a1fd8405714dea50c79bec239b617a9d7
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-05-04 14:46:02 -0700

    Created initialized files for exam questions

---
+++ exam02/q03.hs
@@ -0,0 +1,23 @@
+{-
+Description:
+Haskell file for Exam 2.
+Write a program that allows a user to select a number between 1 and 5 and then prints a famous quote
+(quotes are of your choosing). After printing the quote, the program will ask whether the user would
+like another. If the user enters n, the program ends; otherwise, the user gets another quote. The program
+repeats until the user enters n. Try to use lazy evaluation and treat the user input as a list rather than
+recursively calling main at the end.
+
+Name: Diego Cruz
+SID: 013540384
+
+Course: CS 152
+Section: 01
+Date: 05 May 2023
+
+To compile:
+ghc -o q3 q03.hs
+
+To run:
+./q3
+-}
+
